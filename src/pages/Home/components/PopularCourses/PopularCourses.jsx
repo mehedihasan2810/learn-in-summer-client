b60304564f70904529311fe7b9c69a199a226573
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-06-07 22:27:02 +0600

    added card of popular classes

--- src/pages/Home/components/PopularCourses/PopularCourses.jsx
+++ src/pages/Home/components/PopularCourses/PopularCourses.jsx
@@ -1,4 +1,5 @@
 import { useRef, useState } from "react";
+import { FaRegClock, FaGraduationCap } from "react-icons/fa";
 import "./PopularCourses.css";
 
 const tabs = ["Popular", "Trending", "New", "Rated"];
@@ -29,6 +30,117 @@ const handleTab = (e, index, tab) => {
           ))}
 
           <span ref={underRef} className="under"></span>
+        </div>
+        <div className="card-container">
+
+
+            <div className="card">
+                <img src="https://images.unsplash.com/photo-1588032786045-59cefda005c0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1188&q=80" alt="" />
+                <p className="category">Drum</p>
+                <h4>Basics of smashing drum so that ur neighbour cant sleep</h4>
+                <div className="hrs-learners">
+                    <p> <FaRegClock/> 2 - 3 hrs</p>
+                    <p> <FaGraduationCap/> 7,703 learners</p>
+                </div>
+                <div className="btns">
+                    <button className="btn-secondary">More Info</button>
+                    <button className="btn-primary">Start Learning</button>
+                </div>
+            </div>
+            <div className="card">
+                <img src="https://images.unsplash.com/photo-1588032786045-59cefda005c0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1188&q=80" alt="" />
+                <p className="category">Drum</p>
+                <h4>Basics of smashing drum so that ur neighbour cant sleep</h4>
+                <div className="hrs-learners">
+                    <p> <FaRegClock/> 2 - 3 hrs</p>
+                    <p> <FaGraduationCap/> 7,703 learners</p>
+                </div>
+                <div className="btns">
+                    <button className="btn-secondary">More Info</button>
+                    <button className="btn-primary">Start Learning</button>
+                </div>
+            </div>
+            <div className="card">
+                <img src="https://images.unsplash.com/photo-1588032786045-59cefda005c0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1188&q=80" alt="" />
+                <p className="category">Drum</p>
+                <h4>Basics of smashing drum so that ur neighbour cant sleep</h4>
+                <div className="hrs-learners">
+                    <p> <FaRegClock/> 2 - 3 hrs</p>
+                    <p> <FaGraduationCap/> 7,703 learners</p>
+                </div>
+                <div className="btns">
+                    <button className="btn-secondary">More Info</button>
+                    <button className="btn-primary">Start Learning</button>
+                </div>
+            </div>
+            <div className="card">
+                <img src="https://images.unsplash.com/photo-1588032786045-59cefda005c0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1188&q=80" alt="" />
+                <p className="category">Drum</p>
+                <h4>Basics of smashing drum so that ur neighbour cant sleep</h4>
+                <div className="hrs-learners">
+                    <p> <FaRegClock/> 2 - 3 hrs</p>
+                    <p> <FaGraduationCap/> 7,703 learners</p>
+                </div>
+                <div className="btns">
+                    <button className="btn-secondary">More Info</button>
+                    <button className="btn-primary">Start Learning</button>
+                </div>
+            </div>
+            <div className="card">
+                <img src="https://images.unsplash.com/photo-1588032786045-59cefda005c0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1188&q=80" alt="" />
+                <p className="category">Drum</p>
+                <h4>Basics of smashing drum so that ur neighbour cant sleep</h4>
+                <div className="hrs-learners">
+                    <p> <FaRegClock/> 2 - 3 hrs</p>
+                    <p> <FaGraduationCap/> 7,703 learners</p>
+                </div>
+                <div className="btns">
+                    <button className="btn-secondary">More Info</button>
+                    <button className="btn-primary">Start Learning</button>
+                </div>
+            </div>
+            <div className="card">
+                <img src="https://images.unsplash.com/photo-1588032786045-59cefda005c0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1188&q=80" alt="" />
+                <p className="category">Drum</p>
+                <h4>Basics of smashing drum so that ur neighbour cant sleep</h4>
+                <div className="hrs-learners">
+                    <p> <FaRegClock/> 2 - 3 hrs</p>
+                    <p> <FaGraduationCap/> 7,703 learners</p>
+                </div>
+                <div className="btns">
+                    <button className="btn-secondary">More Info</button>
+                    <button className="btn-primary">Start Learning</button>
+                </div>
+            </div>
+            <div className="card">
+                <img src="https://images.unsplash.com/photo-1588032786045-59cefda005c0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1188&q=80" alt="" />
+                <p className="category">Drum</p>
+                <h4>Basics of smashing drum so that ur neighbour cant sleep</h4>
+                <div className="hrs-learners">
+                    <p> <FaRegClock/> 2 - 3 hrs</p>
+                    <p> <FaGraduationCap/> 7,703 learners</p>
+                </div>
+                <div className="btns">
+                    <button className="btn-secondary">More Info</button>
+                    <button className="btn-primary">Start Learning</button>
+                </div>
+            </div>
+            <div className="card">
+                <img src="https://images.unsplash.com/photo-1588032786045-59cefda005c0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1188&q=80" alt="" />
+                <p className="category">Drum</p>
+                <h4>Basics of smashing drum so that ur neighbour cant sleep</h4>
+                <div className="hrs-learners">
+                    <p> <FaRegClock/> 2 - 3 hrs</p>
+                    <p> <FaGraduationCap/> 7,703 learners</p>
+                </div>
+                <div className="btns">
+                    <button className="btn-secondary">More Info</button>
+                    <button className="btn-primary">Start Learning</button>
+                </div>
+            </div>
+
+
+
         </div>
       </section>
     </div>
