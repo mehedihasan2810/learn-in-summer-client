b2fff844797f2abcbd4cc6c416ae7073d2519895
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-06-07 13:08:26 +0600

    stats section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Slider from './components/Slider/Slider'
+import Stats from './components/stats/Stats'
 
 const Home = () => {
   return (
     <div>
       <Slider/>
+      <Stats/>
     </div>
   )
 }
---
+++ src/pages/Home/components/Stats/Stats.jsx
@@ -0,0 +1,38 @@
+import "./Stats.css";
+const Stats = () => {
+  return (
+    <div className="center-container">
+      <div className="stat-container">
+        <div className="item">
+          <h5>
+            <span>7000+</span>
+          </h5>
+          <p>Expert-authored skill development courses.</p>
+        </div>
+        <div className="item">
+          <h5>
+            <span>20,000+</span>
+          </h5>
+          <p>Students use LearnInSummer to gain extra skill.</p>
+        </div>
+        <div className="item">
+          <h5>
+            <span>15,000+</span>
+          </h5>
+          <p>
+            Gave us reviews not just reviews but positive reviwes check for
+            yourself.
+          </p>
+        </div>
+        <div className="item">
+          <h5>
+            <span>500+</span>
+          </h5>
+          <p>Best teachers from around the world are taking classes.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Stats;
