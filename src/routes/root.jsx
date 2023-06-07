c2367949d1792aa7f920871cefcf22599da050ab
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-06-07 12:37:15 +0600

    added hero slider

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -2,10 +2,17 @@ import {
     createBrowserRouter,
   } from "react-router-dom";
 import RootLayout from "../layouts/RootLayout";
+import Home from "../pages/Home/Home";
 
   export const router = createBrowserRouter([
     {
       path: "/",
       element: <RootLayout/>,
+      children: [
+        {
+          path: '/',
+          element: <Home/>
+        }
+      ]
     },
   ]);
